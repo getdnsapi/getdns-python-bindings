5b762edab5f7a316b7eaba5b32a3a11239f056d5
gmadkat <gmadkat@gmadkat-OptiPlex-960.(none)>
2014-02-15 19:37:28 -0500

    Starting to add code for the tree response, added helper methods. TODO: Code cleanup and add more functionality and error checking

--- pygetdns_util.c
+++ pygetdns_util.c
@@ -37,7 +37,6 @@ extensions_to_getdnsdict(PyDictObject *pydict)
     return newdict;
 }
 
-
 PyObject *
 decode_getdns_response(struct getdns_dict *response)
 {
@@ -77,3 +76,232 @@ decode_getdns_response(struct getdns_dict *response)
     }
     return results;
 }
+
+/*
+ * Helper function to process each answer type
+ */
+int process_data(size_t rec_count,
+		         size_t rr_count,
+		         char* type,
+		         PyObject* resultslist,
+		         struct getdns_list *this_answer,
+		         struct getdns_dict *response)
+{
+    getdns_return_t this_ret;
+    struct getdns_dict *this_rr = NULL;
+    struct getdns_dict *this_rdata = NULL;
+	struct getdns_bindata *this_a_record = NULL;
+
+	printf("process_data %s \n", type);
+    PyObject *resultitem = PyDict_New();
+
+    this_ret = getdns_list_get_dict(
+    		this_answer, rr_count, &this_rr);
+    /* Get the RDATA */
+
+    this_ret = getdns_dict_get_dict(
+    		this_rr, "rdata", &this_rdata);
+
+	this_ret = getdns_dict_get_bindata(
+			this_rdata, type, &this_a_record);
+	if (this_ret == GETDNS_RETURN_NO_SUCH_DICT_NAME)
+	{
+		fprintf(stderr,
+			"Weird: the A record at %d in record at %d "
+			"had no address. Exiting.\n",
+			(int) rr_count, (int) rec_count);
+		getdns_dict_destroy(response);
+		return 0;
+	}
+	char *this_address_str = getdns_display_ip_address(this_a_record);
+	if (this_address_str) {
+		printf("The %s address is %s\n", type, this_address_str);
+		PyObject *addr1 = PyDict_New();
+		PyDict_SetItem(addr1, PyString_FromString(type),
+			  PyString_FromString(this_address_str));
+		PyDict_SetItem(resultitem, PyString_FromString("answer"), addr1);
+		PyList_Append(resultslist, resultitem);
+		free(this_address_str);
+	}
+	return 1;
+}
+
+/*
+ * Error checking helper
+ */
+void error_exit(char* msg, getdns_return_t ret)
+{
+    char error_str[512];
+    if (ret != GETDNS_RETURN_GOOD)  {
+        sprintf(error_str, "%s: %d", msg, ret);
+        PyErr_SetString(getdns_error, error_str);
+    }
+}
+
+/*
+ * Helper function for constructing a tree of PyDicts for the response.
+ */
+PyObject *
+decode_getdns_results_tree_response(struct getdns_dict *response)
+{
+    uint32_t error;
+    struct getdns_list *addr_list;
+    size_t n_addrs;
+    getdns_return_t ret;
+    PyObject *results;
+    PyObject *resultslist;
+    size_t rr_count = 0;
+    size_t rec_count = 0;
+    getdns_return_t this_ret;
+
+    printf("decode_getdns_results_tree_response\n");
+    (void)getdns_dict_get_int(response, "status", &error);
+    if (error != GETDNS_RESPSTATUS_GOOD)  {
+    	error_exit("No answers, return value", error);
+        return NULL;
+    }
+    printf("decode_getdns_results_tree_response1\n");
+
+    if ((ret = getdns_dict_get_list(response, "replies_tree",
+                                    &addr_list)) != GETDNS_RETURN_GOOD)  {
+    	error_exit("Extracting answers failed", ret);
+        return NULL;
+    }
+    printf("decode_getdns_results_tree_response2\n");
+
+    results = PyDict_New();
+    if ((resultslist = PyList_New(0)) == NULL)  {
+       error_exit("Unable to allocate response list", 0);
+        return NULL;
+    }
+    printf("decode_getdns_results_tree_response3\n");
+
+    (void)getdns_list_get_length(addr_list, &n_addrs);
+
+    printf("Num of addrs = %lu\n", n_addrs);
+    for ( rec_count = 0; rec_count < n_addrs; ++rec_count )
+    {
+
+	    struct getdns_dict * this_record;
+	    //TODO: Handle errors for below 2 functions
+        this_ret = getdns_list_get_dict(
+        		addr_list, rec_count, &this_record);
+        /* Get the answer section */
+        struct getdns_list * this_answer;
+        this_ret = getdns_dict_get_list(
+        		this_record, "answer", &this_answer);
+
+        /* Get each RR in the answer section */
+        size_t num_rrs;
+        this_ret = getdns_list_get_length(this_answer, &num_rrs);
+        printf("Num of rrs = %lu\n", num_rrs);
+
+        for (rr_count = 0; rr_count < num_rrs; ++rr_count )
+        {
+            //PyObject *resultitem = PyDict_New();
+            struct getdns_dict *this_rr = NULL;
+            //TODO: Handle errors from the below.
+            this_ret = getdns_list_get_dict(
+            		this_answer, rr_count, &this_rr);
+            if (this_ret != GETDNS_RETURN_GOOD)  {
+                error_exit("getdns_list_get_dict failed", this_ret);
+                return NULL;
+            }
+            /* Get the RDATA */
+            struct getdns_dict * this_rdata = NULL;
+            this_ret = getdns_dict_get_dict(
+            		this_rr, "rdata", &this_rdata);
+            if (this_ret != GETDNS_RETURN_GOOD)  {
+            	error_exit("getdns_get_get_dict rdata failed", this_ret);
+                return NULL;
+            }
+
+            /* Get the RDATA type */
+            uint32_t this_type;
+            this_ret = getdns_dict_get_int(
+            		this_rr, "type", &this_type);
+            if (this_ret != GETDNS_RETURN_GOOD)  {
+            	error_exit("getdns_dict_get_int failed", this_ret);
+                return NULL;
+            }
+
+            printf("Type = %d\n", this_type);
+            /* If it is a valid RR stash the value in PyObject */
+            switch (this_type) {
+
+            case GETDNS_RRTYPE_A:
+            {
+            	if (!process_data(rec_count, rr_count,
+            			          "ipv4_address", resultslist,
+            			          this_answer, response)) {
+            		return NULL;
+            	}
+            	break;
+            }
+            case GETDNS_RRTYPE_AAAA:
+           {
+				if (!process_data(rec_count, rr_count,
+						          "ipv6_address", resultslist,
+								  this_answer, response)) {
+					return NULL;
+				}
+				break;
+            }
+            case GETDNS_RRTYPE_NS:
+           {
+				if (!process_data(rec_count, rr_count,
+						          "nsdname", resultslist,
+								  this_answer, response)) {
+					return NULL;
+				}
+				break;
+            }
+            case GETDNS_RRTYPE_MD:
+            case GETDNS_RRTYPE_MF:
+            case GETDNS_RRTYPE_MB:
+           {
+				if (!process_data(rec_count, rr_count,
+						          "madname", resultslist,
+								  this_answer, response)) {
+					return NULL;
+				}
+				break;
+            }
+            case GETDNS_RRTYPE_CNAME:
+            {
+ 				if (!process_data(rec_count, rr_count,
+ 						          "cname", resultslist,
+ 								  this_answer, response)) {
+ 					return NULL;
+ 				}
+ 				break;
+             }
+            case GETDNS_RRTYPE_TXT:
+            {
+ 				if (!process_data(rec_count, rr_count,
+ 						          "txt_strings", resultslist,
+ 								  this_answer, response)) {
+ 					return NULL;
+ 				}
+ 				break;
+             }
+            case GETDNS_RRTYPE_APL:
+            {
+ 				if (!process_data(rec_count, rr_count,
+ 						          "address_family ", resultslist,
+ 								  this_answer, response)) {
+ 					return NULL;
+ 				}
+ 				break;
+             }
+
+            default:
+            	return NULL;
+            }
+       }
+    }
+
+    PyDict_SetItem(results, PyString_FromString("results_tree"), resultslist);
+    return results;
+}
+
