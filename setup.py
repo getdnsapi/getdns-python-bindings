ab56edcc6fb5e8ceda953ea4dfb3e890f1c09929
Melinda Shore <melinda.shore@nomountain.net>
2016-08-01 12:12:27 -0800

    updated version numbering

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ getdns_module = Extension('getdns',
                     )
 
 setup(name='getdns',
-      version='v1.0beta',
+      version='v1.0.0b1',
       description='Python bindings for getdns',
       long_description=long_description,
       license='BSD',
