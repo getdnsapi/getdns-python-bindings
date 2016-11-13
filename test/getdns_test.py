4caa30da10962d5026d128d9795557266adf3f2b
Melinda Shore <melinda.shore@nomountain.net>
2016-11-13 13:11:39 +0900

    Initial unit test framework checkin

---
+++ test/getdns_test.py
@@ -0,0 +1,46 @@
+import unittest
+import getdns
+
+class TestGetdnsMethods(unittest.TestCase):
+
+    def test_context(self):
+        c = getdns.Context()
+        self.assertIsNotNone(c)
+        del(c)
+
+    def test_sync_address(self):
+        c = getdns.Context()
+        c.resolution_type=getdns.RESOLUTION_STUB
+        r = c.address('www.getdnsapi.net')
+        self.assertEqual(r.status, getdns.RESPSTATUS_GOOD)
+        self.assertTrue('185.49.141.37' in [ x['address_data'] for x in r.just_address_answers ])
+        del(c)
+        del(r)
+
+    def test_sync_service(self):
+        c = getdns.Context()
+        c.resolution_type=getdns.RESOLUTION_STUB
+        r = c.service('_xmpp-server._tcp.jabber.org')
+        self.assertEqual(r.status, getdns.RESPSTATUS_GOOD)
+        del(c)
+        del(r)
+
+    def test_sync_hostname(self):
+        c = getdns.Context()
+        c.resolution_type=getdns.RESOLUTION_STUB
+        r = c.hostname( { 'address_type': 'IPv4', 'address_data': '185.49.141.37' } )
+        self.assertEqual(r.status, getdns.RESPSTATUS_GOOD)
+        del(c)
+        del(r)
+
+    def test_sync_general(self):
+        c = getdns.Context()
+        c.resolution_type=getdns.RESOLUTION_STUB
+        r = c.general('nlnetlabs.nl', request_type=getdns.RRTYPE_NS)
+        self.assertEqual(r.status, getdns.RESPSTATUS_GOOD)
+        del(c)
+        del(r)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
